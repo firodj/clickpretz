bc32f3bf7f039007959790477fa288e7ec5e4fa5
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2023-12-29 08:06:15 +0700

    refactor: pamu

--- main.py
+++ main.py
@@ -8,312 +8,10 @@ from enum import Enum, IntFlag
 from PIL import Image
 import numpy as np
 
-from pretz import defs, zips, strs, crypts, wwww
+from pretz import defs, zips, strs, crypts, wwww, pamu
 
-class PretzPamItem:
-	def __init__(self, pam_section, f):
-		self.pam_section: PretzPam = pam_section
-		self.idx = -1
-		self.f: typing.BinaryIO = f
-		self.chunk_id = None
-		self.mode = 0
-		self.chunk_size = None
-		self.chunk_ofs = None
 
-		self.data_ofs = None
-		self.data_size = None
-		self.encrypted_size = None
-		self.expected_size = None
-		self.compress = ''
 
-	def get_data(self):
-		f = self.f
-		f.seek(self.chunk_ofs, os.SEEK_SET)
-
-		chunk_data_end = self.chunk_ofs + self.chunk_size
-
-		data = None
-		match self.mode:
-			case 1:
-				f.seek(self.data_ofs, os.SEEK_SET)
-				rawdata = f.read(self.data_size)
-				data = zips.decompress(rawdata)
-			case 2:
-				f.seek(self.data_ofs, os.SEEK_SET)
-				data = f.read(self.chunk_size)
-				data = self.pam_section.decryptor.decode(data, self.chunk_id)
-				#print(bytes(out).hex(':'))
-			case 3:
-				f.seek(self.data_ofs, os.SEEK_SET)
-				data = f.read(self.data_size)
-				data = self.pam_section.decryptor.decode(data, self.chunk_id)
-				data = zips.decompress(data[4:])
-				try:
-					print(data.decode('utf-8'))
-				except:
-					print(data.hex(':'))
-			case 4:
-				raise Exception("not yet implemented for lz4")
-			case _:
-				f.seek(self.data_ofs, os.SEEK_SET)
-				data = f.read(self.chunk_size)
-
-		if f.tell() != chunk_data_end:
-			raise Exception("unexpected different position")
-
-		return data
-
-	def info(self):
-		print("---\n%d. chunk id = 0x%x (%s)" % (self.idx, self.chunk_id, defs.chunk_type(self.chunk_id)))
-		print("mode = %d (%s)" % (self.mode, zips.mode_description(self.mode)))
-		print("chunk size = %d (0x%x)" % (self.chunk_size, self.chunk_size))
-		print("chunk ofs = %d (0x%x)" % (self.chunk_ofs, self.chunk_ofs))
-		print("data ofs = %d (0x%x)" % (self.data_ofs, self.data_ofs))
-		if self.encrypted_size:
-			print("encrypted size = %d (0x%x)" % (self.encrypted_size, self.encrypted_size))
-		if self.expected_size:
-			print("expected size = %d (0x%x)" % (self.expected_size, self.expected_size))
-		if self.data_size:
-			print("data size = %d (0x%x)" % (self.data_size, self.data_size))
-
-		print("compress = %s" % (self.compress,))
-
-	def __repr__(self):
-		return 'PretzPamItem(idx=%d,type=%s,chunk_id=0x%04x,mode=%d,size=%d)' % (self.idx, defs.chunk_type(self.chunk_id),
-														self.chunk_id, self.mode, self.chunk_size)
-
-	def cache_file(self):
-		name = "%s_%03d.bin" % (defs.chunk_type(self.chunk_id), self.idx)
-		return open('out/chunks/' + name, "rb")
-
-class PretzPam:
-	def __init__(self, f, start):
-		self.f: typing.BinaryIO = f
-		self.start = start
-		self.unicode = False
-		self.magic = None
-		self.runtime_sub_version = None
-		self.runtime_version = None
-		self.product_version = None
-		self.product_build = None
-		self.new_game = None
-		self.game_mode = None
-		self.entry_ofs = None
-		self.decryptor = None
-		self.items: list[PretzPamItem] = []
-
-	def parse(self):
-		f = self.f
-		f.seek(self.start, os.SEEK_SET)
-
-		self.magic = f.read(4)
-		print("magic = ", self.magic)
-
-		if self.magic == b'PAMU':
-			self.unicode = True
-		elif self.magic == b'PAME':
-			pass
-		else:
-			raise Exception("unexpected pam header %s" % (self.magic,))
-
-		print("unicode game = %s" %(self.unicode,))
-
-		self.runtime_sub_version = struct.unpack('<H', f.read(2))[0]
-		print("runtime sub version = %d (0x%x)" % (self.runtime_sub_version, self.runtime_sub_version))
-
-		self.runtime_version = struct.unpack('<H', f.read(2))[0]
-		print("runtime version = %d (0x%x)" % (self.runtime_version, self.runtime_version))
-
-		self.product_version = struct.unpack('<L', f.read(4))[0]
-		print("product version = %d (0x%x)" % (self.product_version, self.product_version))
-
-		self.product_build = struct.unpack('<L', f.read(4))[0]
-		print("product build = %d (0x%x)" % (self.product_build, self.product_build))
-
-		#entry_ofs = f.tell()
-
-		self.game_mode = "284"
-		self.new_game = False
-		magic_char = 54 # 'c'
-
-		if self.product_build < 284:
-			self.game_mode = "OLD"
-			magic_char = 99 # '6'
-		elif self.product_build > 285:
-			self.game_mode = "288"
-			self.new_game =  True
-
-		self.decryptor = crypts.Crypt(magic_char)
-		self.decryptor.project_build = self.product_build
-		self.decryptor.new_game = self.new_game
-		self.decryptor.game_mode = self.game_mode
-
-		#f.seek(entry_ofs, os.SEEK_SET)
-		i = 0
-		while True:
-			item = self.analyze_chunks(f)
-			if item is None:
-				break
-			item.idx = i
-			self.items.append(item)
-			print(item)
-			i += 1
-
-	def analyze_chunks(self, f):
-		item = PretzPamItem(self, f)
-		start = f.tell()
-
-		bindata = f.read(2)
-		if len(bindata) < 2:
-			if start != f.tell():
-				print("warning: leftover")
-			else:
-				print("congratlation: complete processed")
-			return None
-
-		item.chunk_id = struct.unpack('<H', bindata)[0]
-		item.mode = struct.unpack('<H', f.read(2))[0]
-		item.chunk_size = struct.unpack('<L', f.read(4))[0]
-		item.chunk_ofs = f.tell()
-		chunk_data_end = item.chunk_ofs + item.chunk_size
-
-		data = None
-		match item.mode:
-			case 1:
-				item.expected_size = struct.unpack('<L', f.read(4))[0]
-
-				item.data_size = 0
-				if not self.new_game:
-					if item.chunk_size > 4:
-						item.data_size = item.chunk_size - 4
-				else:
-					item.data_size = struct.unpack('<L', f.read(4))[0]
-
-				item.data_ofs = f.tell()
-
-				data = f.read(2)
-				item.compress =  zips.compress_level(data)
-			case 2:
-				item.data_ofs = f.tell()
-
-			case 3:
-				item.encrypted_size = struct.unpack('<L', f.read(4))[0]
-
-				item.data_size = 0
-				if item.chunk_size > 4:
-					item.data_size = item.chunk_size - 4
-
-				item.data_ofs = f.tell()
-
-				data = f.read(8)
-				data = self.decryptor.decode(data, item.chunk_id)
-
-				item.expected_size = struct.unpack('<L', data[0:4])[0]
-
-				item.compress =  zips.compress_level(data[4:])
-			case 4:
-				raise Exception("not yet implemented for lz4")
-			case _:
-				item.data_ofs = f.tell()
-
-		match defs.chunk_type(item.chunk_id):
-			case 'title':
-				data  = item.get_data()
-				value = self.to_string(data)
-				self.decryptor.title = value
-				print("title = %s" % (value,))
-				print("data =",  data.hex(':'))
-
-			case 'project_path':
-				data  = item.get_data()
-				value = self.to_string(data)
-				self.decryptor.project_path = value
-				print("project_path = %s" % (value,))
-				print("data =",  data.hex(':'))
-
-			case 'copyright':
-				data  = item.get_data()
-				value = self.to_string(data)
-				self.decryptor.copyright = value
-				print("copyright = %s" % (value,))
-				print("data =",  data.hex(':'))
-
-			case None:
-				raise Exception("unknown chunk_id = %04x" % (self.chunk_id,))
-
-		f.seek(chunk_data_end, os.SEEK_SET)
-		return item
-
-	def to_string(self, data):
-		return strs.String(data, self.unicode)
-
-	def testing(self, dump=False):
-		for item in self.items:
-			data = item.get_data()
-
-			match defs.chunk_type(item.chunk_id):
-				case 'title':
-					value = self.to_string(data)
-					print("title = %s" % (value,))
-				case 'title2':
-					print("title2 = %s" % (data,))
-				case 'output_path':
-					value = self.to_string(data)
-					print("output_path = %s" % (value,))
-				case 'project_path':
-					value = self.to_string(data)
-					print("project_path = %s" % (value,))
-				case 'author':
-					value = self.to_string(data)
-					print("author = %s" % (value,))
-				case 'about':
-					value = self.to_string(data)
-					print("about = %s" % (value,))
-				case 'copyright':
-					value = self.to_string(data)
-					print("copyright = %s" % (value,))
-				case 'icon':
-					data_begin = struct.unpack('<L', data[0:4])[0]
-
-					with open('out/icon.ico', "wb") as u:
-						u.write(data)
-				case 'last':
-					pass
-				case None:
-					raise Exception("unknown chunk_id = %04x" % (self.chunk_id,))
-
-			if dump:
-				name = "%s_%03d.bin" % (defs.chunk_type(item.chunk_id), item.idx)
-				print("dump to out/chunks/%s"  %  (name))
-				with open('out/chunks/' + name, "wb") as u:
-					u.write(data)
-
-	def testing_images(self, select_number):
-		with self.items[77].cache_file() as f:
-			imageBank = ImageBank(f)
-			imageBank.parse()
-
-			select_numbers = list([select_number])
-			show_image = None
-			if select_number == -1:
-				select_numbers = range(0, len(imageBank.items))
-
-			print('select_number =', select_number)
-			for select_number in select_numbers:
-				item = imageBank.items[select_number]
-
-				print("create image... idx =", item.idx)
-				pixels = item.get_pixels()
-				img = Image.fromarray(pixels, 'RGBA')
-
-				if len(select_numbers) == 1:
-					show_image = img
-				elif item.experiment_show:
-					if item.idx <= 100: continue
-					show_image = img
-					break
-
-			if show_image: show_image.show()
 
 class ClickPretz:
 	def __init__(self, filename):
@@ -354,7 +52,7 @@ class ClickPretz:
 		if f.tell() != entry_start_ofs:
 			print("warning: entry_start_ofs different")
 
-		self.pam_section = PretzPam(f, entry_start_ofs)
+		self.pam_section = pamu.Section(f, entry_start_ofs)
 		self.pam_section.parse()
 
 def ColorFrom32bitRGBA(d: typing.BinaryIO):
@@ -723,6 +421,33 @@ class ImageBank:
 
 		return item
 
+def testing_images(pam_section, select_number):
+	with pam_section.items[77].cache_file() as f:
+		imageBank = ImageBank(f)
+		imageBank.parse()
+
+		select_numbers = list([select_number])
+		show_image = None
+		if select_number == -1:
+			select_numbers = range(0, len(imageBank.items))
+
+		print('select_number =', select_number)
+		for select_number in select_numbers:
+			item = imageBank.items[select_number]
+
+			print("create image... idx =", item.idx)
+			pixels = item.get_pixels()
+			img = Image.fromarray(pixels, 'RGBA')
+
+			if len(select_numbers) == 1:
+				show_image = img
+			elif item.experiment_show:
+				if item.idx <= 100: continue
+				show_image = img
+				break
+
+		if show_image: show_image.show()
+
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print("missing argument")
@@ -737,4 +462,4 @@ if __name__ == '__main__':
 	clickPretz.analyze()
 	#click_Pretz.wwww_section.testing(False)
 	#clickPretz.pam_section.testing(True)
-	clickPretz.pam_section.testing_images(select_number)
+	testing_images(clickPretz.pam_section, select_number)
---
+++ pretz/pamu.py
@@ -0,0 +1,282 @@
+from typing import BinaryIO, cast
+import os
+from . import zips, defs, crypts, strs
+from struct import unpack, pack
+
+class Item:
+	def __init__(self, pam_section, f: BinaryIO):
+		self.pam_section: Section = pam_section
+		self.idx = -1
+		self.f = f
+		self.chunk_id = None
+		self.mode = 0
+		self.chunk_size = None
+		self.chunk_ofs = None
+
+		self.data_ofs = None
+		self.data_size = None
+		self.encrypted_size: int = None
+		self.expected_size: int = None
+		self.compress = ''
+
+	def get_data(self):
+		f = self.f
+		f.seek(self.chunk_ofs, os.SEEK_SET)
+
+		chunk_data_end = self.chunk_ofs + self.chunk_size
+
+		data = None
+		match self.mode:
+			case 1:
+				f.seek(self.data_ofs, os.SEEK_SET)
+				rawdata = f.read(self.data_size)
+				data = zips.decompress(rawdata)
+			case 2:
+				f.seek(self.data_ofs, os.SEEK_SET)
+				data = f.read(self.chunk_size)
+				data = self.pam_section.decryptor.decode(data, self.chunk_id)
+				#print(bytes(out).hex(':'))
+			case 3:
+				f.seek(self.data_ofs, os.SEEK_SET)
+				data = f.read(self.data_size)
+				data = self.pam_section.decryptor.decode(data, self.chunk_id)
+				data = zips.decompress(data[4:])
+				try:
+					print(data.decode('utf-8'))
+				except:
+					print(data.hex(':'))
+			case 4:
+				raise Exception("not yet implemented for lz4")
+			case _:
+				f.seek(self.data_ofs, os.SEEK_SET)
+				data = f.read(self.chunk_size)
+
+		if f.tell() != chunk_data_end:
+			raise Exception("unexpected different position")
+
+		return data
+
+	def info(self):
+		print("---\n%d. chunk id = 0x%x (%s)" % (self.idx, self.chunk_id, defs.chunk_type(self.chunk_id)))
+		print("mode = %d (%s)" % (self.mode, zips.mode_description(self.mode)))
+		print("chunk size = %d (0x%x)" % (self.chunk_size, self.chunk_size))
+		print("chunk ofs = %d (0x%x)" % (self.chunk_ofs, self.chunk_ofs))
+		print("data ofs = %d (0x%x)" % (self.data_ofs, self.data_ofs))
+		if self.encrypted_size:
+			print("encrypted size = %d (0x%x)" % (self.encrypted_size, self.encrypted_size))
+		if self.expected_size:
+			print("expected size = %d (0x%x)" % (self.expected_size, self.expected_size))
+		if self.data_size:
+			print("data size = %d (0x%x)" % (self.data_size, self.data_size))
+
+		print("compress = %s" % (self.compress,))
+
+	def __repr__(self):
+		return 'Item(idx=%d,type=%s,chunk_id=0x%04x,mode=%d,size=%d)' % (self.idx, defs.chunk_type(self.chunk_id),
+														self.chunk_id, self.mode, self.chunk_size)
+
+	def cache_file(self):
+		name = "%s_%03d.bin" % (defs.chunk_type(self.chunk_id), self.idx)
+		return open('out/chunks/' + name, "rb")
+
+class Section:
+	def __init__(self, f, start):
+		self.f: BinaryIO = f
+		self.start = start
+		self.unicode = False
+		self.magic = None
+		self.runtime_sub_version = None
+		self.runtime_version = None
+		self.product_version = None
+		self.product_build = None
+		self.new_game = None
+		self.game_mode = None
+		self.entry_ofs = None
+		self.decryptor = None
+		self.items: list[Item] = []
+
+	def parse(self):
+		f = self.f
+		f.seek(self.start, os.SEEK_SET)
+
+		self.magic = f.read(4)
+		print("magic = ", self.magic)
+
+		if self.magic == b'PAMU':
+			self.unicode = True
+		elif self.magic == b'PAME':
+			pass
+		else:
+			raise Exception("unexpected pam header %s" % (self.magic,))
+
+		print("unicode game = %s" %(self.unicode,))
+
+		self.runtime_sub_version = unpack('<H', f.read(2))[0]
+		print("runtime sub version = %d (0x%x)" % (self.runtime_sub_version, self.runtime_sub_version))
+
+		self.runtime_version = unpack('<H', f.read(2))[0]
+		print("runtime version = %d (0x%x)" % (self.runtime_version, self.runtime_version))
+
+		self.product_version = unpack('<L', f.read(4))[0]
+		print("product version = %d (0x%x)" % (self.product_version, self.product_version))
+
+		self.product_build = unpack('<L', f.read(4))[0]
+		print("product build = %d (0x%x)" % (self.product_build, self.product_build))
+
+		#entry_ofs = f.tell()
+
+		self.game_mode = "284"
+		self.new_game = False
+		magic_char = 54 # 'c'
+
+		if self.product_build < 284:
+			self.game_mode = "OLD"
+			magic_char = 99 # '6'
+		elif self.product_build > 285:
+			self.game_mode = "288"
+			self.new_game =  True
+
+		self.decryptor = crypts.Crypt(magic_char)
+		self.decryptor.project_build = self.product_build
+		self.decryptor.new_game = self.new_game
+		self.decryptor.game_mode = self.game_mode
+
+		#f.seek(entry_ofs, os.SEEK_SET)
+		i = 0
+		while True:
+			item = self.analyze_chunks(f)
+			if item is None:
+				break
+			item.idx = i
+			self.items.append(item)
+			print(item)
+			i += 1
+
+	def analyze_chunks(self, f):
+		item = Item(self, f)
+		start = f.tell()
+
+		bindata = f.read(2)
+		if len(bindata) < 2:
+			if start != f.tell():
+				print("warning: leftover")
+			else:
+				print("congratlation: complete processed")
+			return None
+
+		item.chunk_id = unpack('<H', bindata)[0]
+		item.mode = unpack('<H', f.read(2))[0]
+		item.chunk_size = unpack('<L', f.read(4))[0]
+		item.chunk_ofs = f.tell()
+		chunk_data_end = item.chunk_ofs + item.chunk_size
+
+		data = None
+		match item.mode:
+			case 1:
+				item.expected_size = unpack('<L', f.read(4))[0]
+
+				item.data_size = 0
+				if not self.new_game:
+					if item.chunk_size > 4:
+						item.data_size = item.chunk_size - 4
+				else:
+					item.data_size = unpack('<L', f.read(4))[0]
+
+				item.data_ofs = f.tell()
+
+				data = f.read(2)
+				item.compress =  zips.compress_level(data)
+			case 2:
+				item.data_ofs = f.tell()
+
+			case 3:
+				item.encrypted_size = unpack('<L', f.read(4))[0]
+
+				item.data_size = 0
+				if item.chunk_size > 4:
+					item.data_size = item.chunk_size - 4
+
+				item.data_ofs = f.tell()
+
+				data = f.read(8)
+				data = self.decryptor.decode(data, item.chunk_id)
+
+				item.expected_size = unpack('<L', data[0:4])[0]
+
+				item.compress =  zips.compress_level(data[4:])
+			case 4:
+				raise Exception("not yet implemented for lz4")
+			case _:
+				item.data_ofs = f.tell()
+
+		match defs.chunk_type(item.chunk_id):
+			case 'title':
+				data  = item.get_data()
+				value = self.to_string(data)
+				self.decryptor.title = value
+				print("title = %s" % (value,))
+				print("data =",  data.hex(':'))
+
+			case 'project_path':
+				data  = item.get_data()
+				value = self.to_string(data)
+				self.decryptor.project_path = value
+				print("project_path = %s" % (value,))
+				print("data =",  data.hex(':'))
+
+			case 'copyright':
+				data  = item.get_data()
+				value = self.to_string(data)
+				self.decryptor.copyright = value
+				print("copyright = %s" % (value,))
+				print("data =",  data.hex(':'))
+
+			case None:
+				raise Exception("unknown chunk_id = %04x" % (self.chunk_id,))
+
+		f.seek(chunk_data_end, os.SEEK_SET)
+		return item
+
+	def to_string(self, data):
+		return strs.String(data, self.unicode)
+
+	def testing(self, dump=False):
+		for item in self.items:
+			data = item.get_data()
+
+			match defs.chunk_type(item.chunk_id):
+				case 'title':
+					value = self.to_string(data)
+					print("title = %s" % (value,))
+				case 'title2':
+					print("title2 = %s" % (data,))
+				case 'output_path':
+					value = self.to_string(data)
+					print("output_path = %s" % (value,))
+				case 'project_path':
+					value = self.to_string(data)
+					print("project_path = %s" % (value,))
+				case 'author':
+					value = self.to_string(data)
+					print("author = %s" % (value,))
+				case 'about':
+					value = self.to_string(data)
+					print("about = %s" % (value,))
+				case 'copyright':
+					value = self.to_string(data)
+					print("copyright = %s" % (value,))
+				case 'icon':
+					data_begin = unpack('<L', data[0:4])[0]
+
+					with open('out/icon.ico', "wb") as u:
+						u.write(data)
+				case 'last':
+					pass
+				case None:
+					raise Exception("unknown chunk_id = %04x" % (self.chunk_id,))
+
+			if dump:
+				name = "%s_%03d.bin" % (defs.chunk_type(item.chunk_id), item.idx)
+				print("dump to out/chunks/%s"  %  (name))
+				with open('out/chunks/' + name, "wb") as u:
+					u.write(data)
